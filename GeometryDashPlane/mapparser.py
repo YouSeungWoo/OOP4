bd32895495c105e69e5ddedd20db35c6f49f7c9c
Hyun-SeungHeon <55629960+Hyun-SeungHeon@users.noreply.github.com>
2019-11-23 21:46:01 +0900

    Add files via upload

---
+++ GeometryDashPlane/mapparser.py
@@ -0,0 +1,34 @@
+import pygame
+from manage import *
+from object import Geo, Spike, Brick
+from maplinker import MapLinker
+
+class MapParser():
+    def __init__(self):
+        
+    
+    def parse(self, list, screen):
+        self.arr = [] # list에 담겨 있는 문자열을 숫자들로 바꿔 줌
+        self.objs = []
+        self.bricks = []
+        self.spikes = []
+        self.map_width = int(list[0])
+        del list[0]
+        for s in list:
+            line = s.split(' ')
+            num = []
+            for i in range(0,5):
+                num.append(int(line[i]))
+            self.arr.append(num)
+            
+        for obj in self.arr:
+            if obj[0] == 0 :
+                temp = Brick(FileSize.brick[obj[1]].value[0], FileSize.brick[obj[1]].value[1], obj[1], obj[2], obj[3], obj[4])
+                self.bricks.append(temp)
+            elif obj[0] == 1 :
+                temp = Spike(FileSize.spike[obj[1]].value[0], FileSize.spike[obj[1]].value[0], obj[1], obj[2], obj[3], obj[4])
+                self.spikes.append(temp)
+        self.objs.append(self.bricks)
+        self.objs.append(self.spikes)
+
+        return (self.objs, self.map_width)
