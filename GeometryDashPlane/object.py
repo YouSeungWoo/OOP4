e4ea70a6193fec3776d25aa65b4e1d5d25452dbf
Hyun-SeungHeon <55629960+Hyun-SeungHeon@users.noreply.github.com>
2019-11-23 14:24:42 +0900

    Add files via upload

--- GeometryDashPlane/object.py
+++ GeometryDashPlane/object.py
@@ -1,26 +1,30 @@
-# -*- coding: cp949 -*-
-
 # 게임에 사용되는 객체를 관리하는 파일
 
-import os, sys, pygame, random, math
+import os, sys, pygame, random
 from manage import *
 from pygame.locals import K_SPACE, Rect
 import numpy as np
+import math
 from collision import *
 
 class Geo(pygame.sprite.Sprite):
     def __init__(self, size_x = -1, size_y = -1, screen = None):
         pygame.sprite.Sprite.__init__(self)
-        
-        self.geo_image, self.geo_image_rect  = load_image(FileName.geo.value, size_x, size_y, None) # 이미지 로드
-        self.image = self.geo_image
-        self.rect = self.image.get_rect()
-        self.rect.center = (width * 0.3, height * 0.7)
         self.screen = screen
-
+        self.geo_image, self.rect  = load_image(FileName.geo.value, size_x, size_y, None) # 이미지 로드
+        self.image= self.geo_image
+        self.x,self.y = self.rect.topleft
+        self.rect.center = (width * 0.3, height * 0.7) #처음 좌표 이동
         self.velocity = 0
         self.isUp = False
     
+    def trans(self):
+        (self.x, self.y) = self.rect.center
+        self.image = pygame.transform.rotate(self.geo_image, math.degrees(math.atan(-self.velocity / x_speed)))
+        self.outline()
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        
     def move(self, layer, gamespeed):
         self.isUP = layer.get_key() # get keypress value
 
@@ -32,22 +36,28 @@ class Geo(pygame.sprite.Sprite):
         self.rad = math.atan(-self.velocity / gamespeed)
 
         if self.isUP:
-            self.velocity -= gravity * (1 - math.atan(-self.velocity / gamespeed))
+            self.velocity -= gravity * (1 - self.rad)
         else:
-            self.velocity += gravity * (1 - math.atan(self.velocity / gamespeed))
-                
-        self.x, self.y = self.rect.center
-        self.rad = math.atan(-self.velocity / gamespeed)
-        self.image = pygame.transform.rotate(self.geo_image, math.degrees(self.rad))
-        self.outline()
-        self.rect = self.image.get_rect()
-        self.rect.center = (self.x, self.y)
+            self.velocity += gravity * (1 - self.rad)
+            
+        self.trans()
+        self.rect.move_ip(0 , self.velocity) # geo_image_rect를 옮겨 줌
         
-
-        self.rect.move_ip(0, self.velocity) # geo_image_rect를 옮겨 줌
-
+        if self.rect.bottom >= height: #밑으로 안 넘어가기
+            if not self.isUP or self.velocity > 0 :
+                self.velocity = 0
+                self.trans()
+                bottom = self.rect.bottom
+                self.rect.move_ip(0, height - bottom)
+        elif self.rect.top <= 0 : #위로 안 넘어가기
+            if self.isUP or self.velocity < 0:
+                self.velocity=0
+                self.trans()
+                top = self.rect.top
+                self.rect.move_ip(0, -top)
+        
         return self.rect.topleft # geo_image_rect의 왼쪽 위의 좌표를 반환
-
+    
     def outline(self):
         self.v = Vector
         self.poly = Hitbox.geo.value
@@ -58,8 +68,39 @@ class Geo(pygame.sprite.Sprite):
         
         pygame.draw.polygon(self.screen, BLACK, self.poly.points, 3)
 
+class Thorn(pygame.sprite.Sprite):
+    def __init__(self, size_x=-1, size_y=-1, screen=None, gamespeed = x_speed):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.thorn_image, self.rect = load_image(FileName.thorn.value, size_x, size_y, None) # 이미지 로드
+        self.image=self.thorn_image
+        self.x, self.y = self.rect.topleft
+        self.velocity = -gamespeed
+        self.rect.left = width
+        self.rect.bottom = 0.98*height
+    
+    def draw(self):
+        self.screen.blit(self.thorn_image,self.rect)
+        
+    def update(self):
+        self.rect.move_ip(self.velocity,0)
+        if self.rect.right<0:
+            self.kill()
+            return (-1, -1)
+        else:
+            return self.rect.topleft
 
-class thorn(pygame.sprite.Sprite):
-    def __init__(self, size_x = -1, size_y = -1, screen = None):
-        pygame.sprite.Sprite.__init__(self)
-        self.thorn_image, self.thorn_image_rect = pygame.image.load(FileName.thorn)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
