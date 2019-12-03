b2d615299445d78d72d3219185b255a56e9a64b8
YouSeungWoo <55529141+YouSeungWoo@users.noreply.github.com>
2019-12-03 11:44:02 +0900

    마우스 클릭 추가

--- GeometryDashPlane/GeometryDash.py
+++ GeometryDashPlane/GeometryDash.py
@@ -3,7 +3,7 @@
 import pygame
 from generation import Generation
 from manage import *
-from object import Geo, Spike, Brick
+from object import ImageCache, Geo, Spike, Brick
 import numpy as np
 import random, copy, os, sys
 from input_layer import input_layer
@@ -14,48 +14,36 @@ class Game():
     def __init__(self):
         pygame.init()
         pygame.key.set_repeat(5,5) # 누르고 있을 것을 대비
-        
-        self.generation = Generation()
-        self.population = self.generation.population
-        self.survivors = 0
-        
+
         self.high_score = 0
         self.n_gen = 0
         self.current_score = 0
         self.gamespeed = x_speed
-        self.bgcolor = BLACK
+        self.bgcolor = WHITE
         
-        self.geo = []
-        self.genomes = []
-        self.layers = []
-        self.genomes, self.layers = self.generation.set_initial_genomes()
-        self.layers.append(input_layer(True))
-    
         self.screen = pygame.display.set_mode(scr_size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Geometry Dash: Plain')
 
         self.maploader = MapLoader(self.screen)
+        self.g_cache = ImageCache()
 
         # 유전정보 생성하기
-        for i in range(self.population):
-            self.geo.append(Geo(FileSize.geo.value[0], FileSize.geo.value[1], self.screen)) # geo 생성
-        self.geo.append(Geo(FileSize.geo.value[0], FileSize.geo.value[1], self.screen))
-        
-        self.survivors = len(self.geo)
-        
-        assert len(self.geo) == len(self.layers)
+        self.geo = [Geo(FileSize.geo.value[0], FileSize.geo.value[1], self.screen)] # geo 생성
+        self.layers = [] # 12/03 추가. AI 모드 선택을 위한 변수
+
+       # assert len(self.geo) == len(self.layers)
 
     def playgame(self):
         # setup initial condition
         game_over = False # gameover flag
-        game_ing = True # game palying flag
+        game_ing = False # game palying flag
+        self.layers = [input_layer(self.mode)] # 12/03 추가. 모드에 따라 레이어 설정
         
         # setup images and fonts
-        sysfont = pygame.font.SysFont(None, 25) # 출력할 문장의 폰트
+        sysfont=pygame.font.SysFont(None, 25) # 출력할 문장의 폰트
         gameover_image = sysfont.render("Game Over...", True, BLACK)
         score_image = sysfont.render("High score : {}     score : {}".format(int(self.high_score), int(self.current_score)), True, WHITE) # color changed
-        gen_image = sysfont.render("Gen : {}   Survivors :  {}".format(self.n_gen, self.survivors), True, WHITE)
 
         # setup sprites
         self.bricks = pygame.sprite.Group()
@@ -65,26 +53,23 @@ class Game():
       
         # initial image draw
         self.screen.fill(self.bgcolor) # default background color setup
-        self.screen.blit(score_image, (width * 0.8, 0)) # 점수판 출력
-        self.screen.blit(gen_image, (width * 0.8, 25))
-        self.screen.blit(self.geo[-1].image, self.geo[-1].rect.topleft)
+        self.screen.blit(score_image, (width * 0.7, 0)) # 점수판 출력
+        self.screen.blit(self.geo[0].image, self.geo[0].rect.topleft)
         pygame.display.update()
-        self.list = []
-        
+
         # game loop
         while not game_over:
-            self.bgcolor = BLACK
-            for i in range(18-len(self.list)):
-                self.list.append(random.uniform(-0.5, 0.5))
-            for idx, ly in enumerate(self.layers): # input check
-                self.inputs = [(self.geo[idx].rect.centery-height*0.5)/(height), self.geo[idx].rad*4 / np.pi]
-                for ii in range(18):
-                    self.inputs.append(self.list[ii])
-                ly.ai.forward(self.inputs)
-                ly.get_input() #  모든 레이어에 대해 입력 확인
+            # i = random.randrange(0, 256) # 눈뽕코드
+            # j = random.randrange(0, 256)
+            # k = random.randrange(0, 256)
+            # self.bgcolor = (i, j, k)
+            x, y = self.geo[0].get_xy()
+            for ly in self.layers: # input check
+                ly.get_input(x, y) #  모든 레이어에 대해 입력 확인
+
             if game_ing: # playing loop
                 self.screen.fill(self.bgcolor) #draw background
-                self.list = []
+
                 if self.maploader.check_scroll(self.gamespeed):
                     objs = self.maploader.get_obj()
                     for o in objs[0]:
@@ -92,38 +77,31 @@ class Game():
                     for o in objs[1]:
                         if o.is_collidable():
                             self.spikes.add(o)
-                templist = sorted(self.spikes.sprites(), key = lambda x: x.rect.centerx)
-                templist1 = [x for x in templist if x.rect.centerx > width*0.3]
-                for iii in range(len(templist1)):
-                    self.list.append((templist1[iii].rect.centery-0.5*height)/(height))
+                
                 self.current_score += 0.15
-                score_image = sysfont.render("High score : {}   score : {}".format(int(self.high_score), int(self.current_score)), True, WHITE)
-                gen_image = sysfont.render("Gen : {}   Survivors :  {}".format(self.n_gen, self.survivors), True, WHITE)
+                score_image = sysfont.render("High score : {}     score : {}".format(int(self.high_score), int(self.current_score)), True, WHITE)
                 self.bricks.update()
                 self.spikes.update()
+                # self.screen.blit(self.start_image, (0, 0))
                 self.spikes.draw(self.screen)
                 self.bricks.draw(self.screen)
+                
                 for idx, geo in enumerate(self.geo): # 모든 geo에 대해서 입력 처리 및 그리기 작업 수행
-                    if not geo.isDead:
-                        self.screen.blit(geo.image, geo.move(self.layers[idx], self.gamespeed)) # self.geo.move(key, gamespeed)를 이용해서 geo를 이동시키고 그것을 출력
-              #          pygame.draw.rect(self.screen, WHITE, (geo.rect.left, geo.rect.top, geo.rect.width, geo.rect.height), 10)
-                for geo in self.geo:
-                    if not geo.isDead:
-                        if geo.colli_Check(self.spikes):
-                            geo.isDead = True
-                            self.survivors -= 1
-                self.screen.blit(score_image, (width * 0.8, 0)) # 점수판 출력
-                self.screen.blit(gen_image, (width * 0.8, 25))
+                    self.screen.blit(geo.image, geo.move(self.layers[idx], self.gamespeed)) # self.geo.move(key, gamespeed)를 이용해서 geo를 이동시키고 그것을 출력
+                    pygame.draw.rect(self.screen, WHITE, (geo.rect.left, geo.rect.top, geo.rect.width, geo.rect.height), 10)
+                if self.geo[0].colli_Check(self.spikes):
+                    game_ing = False
+                    game_over = True
+                    self.geo[0].rect.center = (width * 0.3, height * 0.7)
+                    self.geo[0].velocity = 0
+                self.screen.blit(score_image, (width * 0.7, 0)) # 점수판 출력
                 pygame.display.update()
                 self.clock.tick(FPS)
-             #   print(self.survivors)
-                if self.survivors == 0:
-                    game_over = True
-                    break
-       #     else: # game start check
-       #         if (self.layers[-1].get_key() and self.layers[-1].usermode == True) or self.layers[-1].usermode == False:
-       #             game_ing = True # game start
-       #             self.bgcolor = BLACK # background set
+                
+            else: # game start check
+                if (self.layers[0].get_key() and self.layers[0].usermode == True) or self.layers[0].usermode == False:
+                    game_ing = True # game start
+                    self.bgcolor = BLACK # background set
         # game over : out of game loop
         if self.current_score > self.high_score:
             self.high_score = self.current_score
@@ -135,19 +113,6 @@ class Game():
         pygame.display.update()
         self.clock.tick(1)
         self.n_gen += 1
-        
-        self.geo = []
-        
-        for i in range(self.population):
-            self.geo.append(Geo(FileSize.geo.value[0], FileSize.geo.value[1], self.screen)) # geo 생성
-        self.geo.append(Geo(FileSize.geo.value[0], FileSize.geo.value[1], self.screen))
-        self.survivors = len(self.geo)
-        
-        layers = self.generation.set_genomes(self.genomes)
-        for i, l in enumerate(layers):
-            self.layers[i].set_ai(l.ai)
-        self.generation.keep_best_genomes()
-        self.genomes = self.generation.mutations()
 
     def intro(self, user_mode):
         game_start = False
@@ -155,18 +120,25 @@ class Game():
 
         while not game_start:
             self.screen.fill(BLACK)
+            self.print_intro() # 12/03 추가. AI 모드를 위해 위치 조정
+
             if pygame.display.get_surface() == None:
                 print("Couldn't load display surface")
-                return True;
+                return True
             else:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         sys.exit()
                     elif event.type == pygame.KEYDOWN:
-                        game_start = True 
-            
-            self.print_intro() # 11/25추가. 이미지 출력부 분리
+                        self.mode = True # 12/03 추가. AI 모드 인덱스
+                        game_start = True
+                    elif event.type == pygame.MOUSEBUTTONDOWN: # 12/03 추가. AI 모드 마우스 클릭으로 실행
+                        print("hi")
+                        x, y = event.pos
+                        if self.team_image_rect.collidepoint(x, y):
+                            self.mode = False
+                            game_start = True
 
             pygame.display.update()
                         # 아무 키나 누르면 스테이지 생성하고 geo 출력해서 게임 시작
@@ -174,15 +146,19 @@ class Game():
             
         return True
 
+    # course_image 추가
     def print_intro(self):
-        self.start_image, self.start_image_rect = load_image(FileName.background.value, FileSize.background.value[0], FileSize.background.value[1], -1) # sysfont.render("Press any key to Start...", True, (255,255,255))   
+        self.start_image, self.start_image_rect = self.g_cache.load_image(FileName.background.value, FileSize.background.value[0], FileSize.background.value[1], -1) # sysfont.render("Press any key to Start...", True, (255,255,255))   
         self.screen.blit(self.start_image, (0, 0))
-        self.intro_image, self.intro_image_rect = load_image(FileName.title.value, FileSize.title.value[0], FileSize.title.value[1], -1)
+        self.intro_image, self.intro_image_rect = self.g_cache.load_image(FileName.title.value, FileSize.title.value[0], FileSize.title.value[1], -1)
         self.screen.blit(self.intro_image, (width * 0.05, height * 0.1))
-        self.start_txt_image, self.start_txt_rect = load_image(FileName.start_txt.value, FileSize.start_txt.value[0], FileSize.start_txt.value[1], -1)
+        self.start_txt_image, self.start_txt_rect = self.g_cache.load_image(FileName.start_txt.value, FileSize.start_txt.value[0], FileSize.start_txt.value[1], -1)
         self.screen.blit(self.start_txt_image, (width * 0.3, height * 0.7))
-        self.team_image, self.team_image_rect = load_image(FileName.team_name.value, FileSize.team_name.value[0], FileSize.team_name.value[1], -1)
+        self.team_image, self.team_image_rect = self.g_cache.load_image(FileName.team_name.value, FileSize.team_name.value[0], FileSize.team_name.value[1], -1)
         self.screen.blit(self.team_image, (width * 0.88, height * 0.85))
+        self.team_image_rect.topleft = (width * 0.88, height * 0.85) # 12/03 추가. AI 모드 마우스 클릭으로 실행을 위해 rect 위치 조정
+        self.course_image, self.course_image_rect = self.g_cache.load_image(FileName.course.value, FileSize.course.value[0], FileSize.course.value[1], -1) # 11/28 추가
+        self.screen.blit(self.course_image, (width * 0.03, height * 0.85))
 
     def start(self):
         is_start = self.intro(True)
